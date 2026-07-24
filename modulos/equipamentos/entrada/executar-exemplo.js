1b6c20f6cdea43124c48145f9a7103ec42cb1c3a
Gabriel Nascimento <99542800+gabinpoa@users.noreply.github.com>
2026-07-24 17:06:37 +0000

    Update executar-exemplo add tipo in dto

--- modulos/equipamentos/entrada/executar-exemplo.js
+++ modulos/equipamentos/entrada/executar-exemplo.js
@@ -2,6 +2,7 @@ import { EquipamentoRepositoryMemoria } from '../infraestrutura/equipamento-repo
 import { CadastrarEquipamento } from '../aplicacao/casos-de-uso/cadastrar-equipamento.js';
 import { GeradorIdFixo } from '../../../compartilhado/ids/gerador-id-fixo.js';
 import { RelogioFixo } from '../../../compartilhado/relogio/relogio-fixo.js';
+import { TipoEquipamento } from '../dominio/tipo-equipamento.js';
 const repository=new EquipamentoRepositoryMemoria();
 const caso=new CadastrarEquipamento(repository,new GeradorIdFixo(['equipamentos-001']),new RelogioFixo());
-console.log('\n[EQUIPAMENTOS]',await caso.executar({ serviceTag:'ABC1234', modelo:'Latitude 7450', clienteId:'cliente-001' }));
+console.log('\n[EQUIPAMENTOS]',await caso.executar({ serviceTag:'ABC1234', modelo:'Latitude 7450', clienteId:'cliente-001', tipo: TipoEquipamento.NOTEBOOK }));
