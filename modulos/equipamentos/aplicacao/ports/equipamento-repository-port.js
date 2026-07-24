aa5ff0808c0f057f9312acbac4dd3691ae5b2e0f
Andre Luis Cabral Dias da Silva <AndreSilva25@edu.unisinos.br>
2026-07-24 14:23:22 -0300

    Ports folder and files updated

---
+++ modulos/equipamentos/aplicacao/ports/equipamento-repository-port.js
@@ -0,0 +1,6 @@
+export class EquipamentoRepositoryPort {
+  async salvar(){ throw new Error('METODO_NAO_IMPLEMENTADO'); }
+  async buscarPorId(){ throw new Error('METODO_NAO_IMPLEMENTADO'); }
+  async listar(){ throw new Error('METODO_NAO_IMPLEMENTADO'); }
+  async buscarPorChave(){ throw new Error('METODO_NAO_IMPLEMENTADO'); }
+}
